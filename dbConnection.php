1dcb1c2d06cf61e5d2c4a68d86298c199432436f
misternani <132259602+misternani@users.noreply.github.com>
2023-09-07 15:16:59 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,3 +1,3 @@
 <?php
-	$con= new mysqli('localhost','root','','college')or die("Could not connect to mysql".mysqli_error($con));
+	$con= new mysqli('localhost','root','jDdGcH3m6svxhd','college')or die("Could not connect to mysql".mysqli_error($con));
 ?>
